901dd572888c5ed3c2e33b6c97eb11816ebbdeb6
seth-ever <72983810+seth-ever@users.noreply.github.com>
2022-04-20 18:14:49 -0500

    Update rankedListGenerator.cpp
    This is no longer the main file that is executed, instead will be executed out of main.cpp
    A header file has been added for this file, and this file has been appended acordingly.

--- rankedListGenerator.cpp
+++ rankedListGenerator.cpp
@@ -1,69 +1,90 @@
-#include <iostream>
-#include <vector>
-#include <fstream>
+#include "rankedListGenerator.h"
 using namespace std;
 
-void populatRandom(vector<string>& array){//Values 0-199
-    for (size_t i = 0; i < 30; i++){
+void populatRandom(vector<string> &array)
+{ // Values 0-199
+    for (size_t i = 0; i < 30; i++)
+    {
         array.push_back(to_string(rand() % 200));
     }
 }
 
-void populateVector(vector<string>& array,string fileName){
-    if (fileName == "Random"){ //Purely For test Implementaion, 
+void populateVector(vector<string> &array, string fileName)
+{
+    if (fileName == "Random")
+    { // Purely For test Implementaion,
         populatRandom(array);
         fileName += ".txt";
         return;
     }
     ifstream inpuStream;
     inpuStream.open(fileName);
-    if (!inpuStream.is_open()){
+    if (!inpuStream.is_open())
+    {
         throw std::invalid_argument("File does not exist.\n");
     }
     string line;
-    while (getline(inpuStream, line)){
-        if (line != "") array.push_back(line);
+    while (getline(inpuStream, line))
+    {
+        if (line != "" && line != "\n" && isalnum(line[0]))
+            array.push_back(line);
     }
     inpuStream.close();
 }
 
-bool compare(string left, string right){// Returns false if left is better than right, true otherwise. Better quality < Worse quality.
+bool compare(string left, string right)
+{ // Returns false if left is better than right, true otherwise. Better quality < Worse quality.
     static int compCount = 0;
     int answer;
-    if (left == right) return false;
+    if (left == right)
+        return false;
     compCount++;
-    while (true){ 
+    while (true)
+    {
         cout << "CompCount " << compCount << endl;
-        cout << "Which item is better:" << endl << "1. " << left << endl <<"2. " << right << endl;
+        cout << "Which item is better:" << endl
+             << "1. " << left << endl
+             << "2. " << right << endl;
         cin >> answer;
-        cout << endl << endl << endl;
-        if (answer == 1) return false;
-        else if (answer == 2) return true;
-        else cout << "Invalid Response" << endl;
+        cout << endl
+             << endl
+             << endl;
+        if (answer == 1)
+            return false;
+        else if (answer == 2)
+            return true;
+        else
+            cout << "Invalid Response" << endl;
     }
-    
 }
 
-void printList(vector<string> list){//For testing Purposes.
-    for (size_t i = 0; i < list.size(); i++){
+void printList(vector<string> list)
+{ // For testing Purposes.
+    for (size_t i = 0; i < list.size(); i++)
+    {
         cout << list.at(i) << endl;
     }
 }
 
-int partition(vector<string> &items, int left, int right) {//iterates through vector, (sub-vector) seperates as greater than/less than pivot.
+int partition(vector<string> &items, int left, int right)
+{ // iterates through vector, (sub-vector) seperates as greater than/less than pivot.
     int pivotIndex = left + (right - left) / 2;
     string pivotValue = items[pivotIndex];
     int i = left, j = right;
     string temp;
-    while(i <= j) {
-        while(compare(pivotValue, items[i] )) { // pivotValue > items[i]
+    while (i <= j)
+    {
+        while (compare(pivotValue, items[i]))
+        { // pivotValue > items[i]
             i++;
         }
-        while(compare(items[j], pivotValue)) { //items[j] > pivotValue
+        while (compare(items[j], pivotValue))
+        { // items[j] > pivotValue
             j--;
         }
-        if(i <= j) {
-            swap(items[i],items[j]);
+        if (i <= j)
+        {
+            swap(items[i], items[j]);
             i++;
             j--;
         }
@@ -71,61 +92,73 @@ int partition(vector<string> &items, int left, int right) {//iterates through ve
     return i;
 }
 
-void quicksort(vector<string> &items, int left, int right) { //Invokes the partion, recurcive
-    if(left < right) {
+void quicksort(vector<string> &items, int left, int right)
+{ // Invokes the partion, recurcive
+    if (left < right)
+    {
         int pivotIndex = partition(items, left, right);
         quicksort(items, left, pivotIndex - 1);
         quicksort(items, pivotIndex, right);
     }
 }
 
-vector<string> quicksort2(vector<string> items){//more spacially complex but perhaps faster.
+vector<string> quicksort2(vector<string> items)
+{ // more spacially complex but perhaps faster.
     vector<string> result;
-    if (items.size() == 1){
+    if (items.size() == 1)
+    {
         result.push_back(items[0]);
         return result;
     }
-    if (items.size() == 0){ return result;}
+    if (items.size() == 0)
+    {
+        return result;
+    }
     vector<string> left;
     vector<string> right;
     string pivotValue = items[0];
-    for (size_t i = 1;i <items.size(); i++){
-        if (compare(items[i], pivotValue)) {right.push_back(items[i]);} //items[i] > pivotValue
-        else {left.push_back(items[i]);}// items[i] < pivotValue
+    for (size_t i = 1; i < items.size(); i++)
+    {
+        if (compare(items[i], pivotValue))
+        {
+            right.push_back(items[i]);
+        } // items[i] > pivotValue
+        else
+        {
+            left.push_back(items[i]);
+        } // items[i] < pivotValue
     }
     vector<string> sortedLeft = quicksort2(left);
     vector<string> sortedRight = quicksort2(right);
 
-    result.insert(result.end(),sortedLeft.begin(),sortedLeft.end());
+    result.insert(result.end(), sortedLeft.begin(), sortedLeft.end());
     result.push_back(pivotValue);
-    result.insert(result.end(),sortedRight.begin(),sortedRight.end());
+    result.insert(result.end(), sortedRight.begin(), sortedRight.end());
     return result;
-
 }
 
-void archiveSortedList(vector<string> sortedItems, string fileName){
-    string newFileName = "Ranked_" + fileName;
-    ofstream outputStream(newFileName);
-    for (size_t i = 0; i < sortedItems.size(); i++){
+void archiveSortedList(vector<string> sortedItems, string fileName)
+{
+
+    ofstream outputStream(fileName);
+    for (size_t i = 0; i < sortedItems.size(); i++)
+    {
         outputStream << i + 1 << ". " << sortedItems.at(i) << endl;
     }
     outputStream.close();
 }
 
-int main(){
+void generator()
+{
     string fileName;
     cout << "Enter the file name of the unsorted list." << endl;
     cin >> fileName;
     vector<string> items;
 
-    populateVector(items, fileName); //printList(items);
-            
-    
-    //quicksort(items, 0, items.size() - 1);
+    populateVector(items, fileName); // printList(items);
+
+    // quicksort(items, 0, items.size() - 1);
     items = quicksort2(items);
-
+    fileName = "Ranked_" + fileName;
     archiveSortedList(items, fileName);
-    
-    return 0;
 }
-
